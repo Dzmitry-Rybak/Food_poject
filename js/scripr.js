1c6cd4dd0fee437e318b2a5427db4f9eb8a6930b
Dzmitry Rybak <nakadisa@icloud.com>
2023-03-03 17:19:56 +0100

    chanded XMLHttpRequest to Fetch API

--- js/scripr.js
+++ js/scripr.js
@@ -200,7 +200,7 @@ window.addEventListener('DOMContentLoaded', () => {
         430
     ).render()
 
-        // Forms  using XMLHttpRequest
+        // Forms  using fetch API
 
         const forms = document.querySelectorAll('form');
         
@@ -226,24 +226,31 @@ window.addEventListener('DOMContentLoaded', () => {
                 ` ;
                 form.insertAdjacentElement('afterend', statusMessage);
 
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
 
-                // request.setRequestHeader('Content-type', 'multipart/form-data');
                 const formData = new FormData(form);
                 
-                request.send(formData);
-                
-                request.addEventListener('load', () => {
-                    if (request.status === 200) {
-                        console.log(request.response);
-                        showThanksModal(message.success);
-                        form.reset();
-                        statusMessage.remove();
-                    } else {
-                        showThanksModal(message.failure);
-                    }
+                const object = {};
+                formData.forEach(function(value,key) {
+                    object[key] = value;
                 })
+
+                fetch('server.php', {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(object)
+                })
+                .then(data => data.text())
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset();
+                });
             });
         }
 
